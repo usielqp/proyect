3a95ef6c79236066285b01988f2afa79ae7ac0dc
fabri1221 <quisperupayllafabrizio@gmail.com>
2024-05-15 12:01:37 -0500

    Update PROYECT.java
    Aqui actualizando el codigo

--- src/proyect/PROYECT.java
+++ src/proyect/PROYECT.java
@@ -30,6 +30,7 @@ public class PROYECT {
         // HACEMOS LAS OPERACIONES EN CONDICIONALES
         switch(opcion){
             case 1 :
+              System.out.println("Hola mundo");
                 System.out.print("digite el primer numero:");
                 numero1=sc.nextInt();
                 
